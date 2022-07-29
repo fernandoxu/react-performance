f708437b59bedc022dba1f83e23b920ff515860f
fernandoxu <fernandoxu9@gmail.com>
2022-07-29 09:21:45 +0800

    02e2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,7 +4,7 @@
 import * as React from 'react'
 import {useCombobox} from '../use-combobox'
 import {getItems} from '../filter-cities'
-import {useForceRerender} from '../utils'
+import {useAsync, useForceRerender} from '../utils'
 
 function Menu({
   items,
@@ -60,8 +60,14 @@ function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
 
-  // 🐨 wrap getItems in a call to `React.useMemo`
-  const allItems = React.useMemo(() => getItems(inputValue), [inputValue])
+  const {data: allItems, run} = useAsync({
+    data: [],
+    status: 'pending',
+  })
+
+  React.useEffect(() => {
+    run(getItems(inputValue))
+  }, [inputValue, run])
   const items = allItems.slice(0, 100)
 
   const {
